d605f8a3830ff8d055e8fe0dc6af893e820947c7
Rory Jarrard <rjarrard@gmail.com>
2025-01-31 13:06:17 -0700

    switch to input from signals

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,12 +10,15 @@ import { DUMMY_USERS } from '../../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  imagePath = computed(() => `assets/users/${this.avatar()}`);
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
 
   onSelectUser() {
 
