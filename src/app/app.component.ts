aca3e337a35185786ccdb5e895572ca3786f9d58
Rory Jarrard <rjarrard@gmail.com>
2025-01-31 15:43:02 -0700

    tasks component mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 import { DUMMY_USERS } from './dummy-users';
 
@@ -14,14 +15,15 @@ interface User {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users: User[] = DUMMY_USERS;
+  selectedUser: User | null = null;
 
   onSelectUser(id: string) {
-    console.log('User selected:', id);
+    this.selectedUser = this.users.find((user) => user.id === id) || null;
   }
 }
