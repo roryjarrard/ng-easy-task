653ac672ec736a093249505a7e1b7265d7d6b40c
Rory Jarrard <rjarrard@gmail.com>
2025-02-04 14:17:09 -0700

    new task mvp

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import type { Task } from './task/task.model';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name?: string;
+  showAddTask = false;
 
   tasks: Task[] = [
     {
@@ -45,5 +47,9 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.showAddTask = true;
   }
 }
