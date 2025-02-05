4d6e5fb47c4414f5a2128c6d74907be2e6eaca99
Rory Jarrard <rjarrard@gmail.com>
2025-02-05 11:11:36 -0700

    add constructor

--- src/app/components/tasks/tasks.service.ts
+++ src/app/components/tasks/tasks.service.ts
@@ -29,6 +29,14 @@ export class TasksService {
         },
       ];
 
+      constructor() {
+        const tasks = localStorage.getItem('tasks');
+
+        if (tasks) {
+          this.tasks = JSON.parse(tasks);
+        }
+      }
+
       getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
       }
@@ -39,9 +47,15 @@ export class TasksService {
             userId: userId,
             ...taskData,
           });
+          this.saveTasks();
       }
 
       removeTask(taskId: string) {
         this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasks();
+      }
+
+      private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
 }
