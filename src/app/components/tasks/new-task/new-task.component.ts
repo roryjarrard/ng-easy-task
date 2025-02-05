c43f126ef3b0f6322a6f1eec7e7040e5b8ba6056
Rory Jarrard <rjarrard@gmail.com>
2025-02-05 11:01:45 -0700

    add tasks service

--- src/app/components/tasks/new-task/new-task.component.ts
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NewTaskData } from '../task/task.model';
+import type { NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,18 +11,20 @@ import { NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  
+  private tasksService = inject(TasksService);
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onCreateTask() {
-    console.log(`Creating task with title: ${this.enteredTitle}, summary: ${this.enteredSummary}, due date: ${this.enteredDate}`);
-    this.add.emit({ title: this.enteredTitle, summary: this.enteredSummary, dueDate: this.enteredDate } as NewTaskData);
+    this.tasksService.addTask(this.userId, { title: this.enteredTitle, summary: this.enteredSummary, dueDate: this.enteredDate } as NewTaskData);
+    this.close.emit();
   }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import type { NewTaskData, Task } from './task/task.model';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -13,56 +14,24 @@ import { NewTaskComponent } from './new-task/new-task.component';
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name?: string;
+  private tasksService = inject(TasksService);
   showAddTask = false;
 
-  tasks: Task[] = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== taskId);
-  }
+  // onCompleteTask(taskId: string) {
+  //   // this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  // }
 
   onStartAddTask() {
     this.showAddTask = true;
   }
 
-  onCancelAddTask() {
-    this.showAddTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      ...taskData,
-    });
+  onCloseAddTask() {
     this.showAddTask = false;
   }
 }
